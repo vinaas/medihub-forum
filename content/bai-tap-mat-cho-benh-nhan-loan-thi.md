86f504dfe1f03b89ccd2252d630922fd421dd8f6
tungptvn <tungpt.hd@gmail.com>
2016-08-21 19:59:36 +0700

    new file:   content/bai-tap-mat-cho-benh-nhan-loan-thi.md
    modified:   content/bai-tap-mat-cho-benh-nhan-loan-thi.md
    
    modified:   handlebars-helper.js
    
    modified:   layout/4rum-chuyentrang.category.html
    
    modified:   layout/4rum-chuyentrang.html
    
    modified:   layout/forum-chuyende.category.html
    
    modified:   layout/forum-chuyenmuc.category.html

---
+++ content/bai-tap-mat-cho-benh-nhan-loan-thi.md
@@ -0,0 +1,18 @@
+---json
+{
+    "slug": "bai-tap-mat-cho-benh-nhan-loan-thi",
+    "category": "cac-benh.mat.loan-thi",
+    "tag": [],
+    "layout": "forum-cauhoi.html",
+    "title": "Bài tập mắt cho bệnh nhân loạn thị",
+    "description": "Các bài tập mắt không chỉ giúp bệnh nhân loạn thị cải thiện thị lực của mình, nó còn giúp cho những người thường xuyên phải đọc tài liệu hoặc sử dụng máy tính có một đôi mắt khỏe đẹp như mong muốn.",
+    "date": "2016-08-21 19:58:50",
+    "thoi_gian": "21-08-2016",
+    "luot_xem": "123",
+    "phan_hoi": "",
+    "yeu_thich": "",
+    "nguoi_dang": "Tung",
+    "hinh_anh": "img/tuyet-chieu-tri-dau-mat-do-cuc-huu-hieu-bang-khoai-tay-mat-ong.jpeg"
+}
+---
+Các bài tập mắt không chỉ giúp bệnh nhân loạn thị cải thiện thị lực của mình, nó còn giúp cho những người thường xuyên phải đọc tài liệu hoặc sử dụng máy tính có một đôi mắt khỏe đẹp như mong muốn.
--- handlebars-helper.js
+++ handlebars-helper.js
@@ -1,20 +1,19 @@
 'use strict';
 
 var HandlebarsLayouts = require('handlebars-layouts');
+var Helpers = require('handlebars-helpers');
 var moment = require('moment');
-var helpers = require('handlebars-helpers')();
 
 module.exports = function (Handlebars) {
     Handlebars.registerHelper(HandlebarsLayouts(Handlebars));
-     //add github handlebar helper
-     Handlebars.registerHelper(helpers);
+  //  Helpers({handlebars: Handlebars});
+    ['array', 'code', 'collection', 'comparison', 'date', 'fs', 'html', 'i18n', 'inflection', 'logging', 'markdown', 'match', 'math', 'misc', 'number', 'path', 'string', 'url'].forEach(function(name) {
+        Helpers[name]({
+        handlebars: Handlebars
+        });
+    });
+
     // dang ky rivetData helper block cho handlebars ở đây
-    Handlebars.registerHelper('ifCond', function(v1, v2, options) {
-        if(v1 === v2) {
-          return options.fn(this);
-        }
-        return options.inverse(this);
-      });
 
     // rivetData helper, bat buoc key trong meta data cua content phai la 'rivetData'
     Handlebars.registerHelper('rivetData', obj => {
@@ -23,13 +22,48 @@ module.exports = function (Handlebars) {
         else
             return '{}';
     });
+    Handlebars.registerHelper('ifCond', function(v1, v2, options) {
+        if(v1 === v2) {
+          return options.fn(this);
+        }
+        return options.inverse(this);
+      });
 
     Handlebars.registerHelper('json', function (obj) {
         return JSON.stringify(obj);
     });
+    
+     Handlebars.registerHelper('toString', function (obj) {
+        return obj.toString();
+    });
 
     Handlebars.registerHelper('removeIndex', function (url) {
         return url.replace('index.html', '');
+    });
+    
+    // child path must be a fulll path e.g. 'tin-tuc.tin-van-hoa', 'tin-tuc.tin-the-thao.quoc-te'
+    /*
+    var data = {
+		'tin-tuc': { files: ['1', '2', '3'], ...},
+		'tin-tuc.tin-van-hoa': { files: ['4', '4', '4'], ...},
+		'tin-tuc.tin-the-thao': { files: ['5', '5', '5'], ...},
+		'tin-tuc.tin-the-thao.quoc-te': { files: ['7', '7', '7'], ...},
+		'tin-tuc.tin-kinh-te': { files: ['6', '6', '6'], ...},
+	}
+	(lookupChild data  'tin-tuc.tin-the-thao')
+	result: { files: ['7', '7', '7'], ...}
+    */
+    Handlebars.registerHelper('lookupChild', function (obj, childPath) {
+		var ret = [];
+		for (var key in obj) {
+			if (!obj.hasOwnProperty(key)) continue;
+			if (key.startsWith(childPath)) {
+				var chunks = key.substr(childPath.length).split('.');
+				if (chunks.length > 1)
+					ret.push(obj[key]);
+			}
+		}
+		return ret;
     });
 
     Handlebars.registerHelper('formatDate', function (context, options) {
